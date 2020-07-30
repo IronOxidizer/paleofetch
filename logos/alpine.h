12ffdb3b1fe4022555be5846455137878aa8331b
Iron Oxidizer <ironoxidizer@gmail.com>
2020-07-30 17:41:41 -0400

    Fixed logo

---
+++ logos/alpine.h
@@ -0,0 +1,24 @@
+// This was stolen from neofetch
+char *LOGO[] = {
+    "       .hddddddddddddddddddddddh.          ",
+    "      :dddddddddddddddddddddddddd:         ",
+    "     /dddddddddddddddddddddddddddd/        ",
+    "    +dddddddddddddddddddddddddddddd+       ",
+    "  `sdddddddddddddddddddddddddddddddds`     ",
+    " `ydddddddddddd++hdddddddddddddddddddy`    ",
+    ".hddddddddddd+`  `+ddddh:-sdddddddddddh.   ",
+    "hdddddddddd+`      `+y:    .sddddddddddh   ",
+    "ddddddddh+`   `//`   `.`     -sddddddddd   ",
+    "ddddddh+`   `/hddh/`   `:s-    -sddddddd   ",
+    "ddddh+`   `/+/dddddh/`   `+s-    -sddddd   ",
+    "ddd+`   `/o` :dddddddh/`   `oy-    .yddd   ",
+    "hdddyo+ohddyosdddddddddho+oydddy++ohdddh   ",
+    ".hddddddddddddddddddddddddddddddddddddh.   ",
+    " `yddddddddddddddddddddddddddddddddddy`    ",
+    "  `sdddddddddddddddddddddddddddddddds`     ",
+    "    +dddddddddddddddddddddddddddddd+       ",
+    "     /ddddddddddddddddddddddddddd/         ",
+    "      :dddddddddddddddddddddddddd:         ",
+    "       .hddddddddddddddddddddddh.          "
+};
+
