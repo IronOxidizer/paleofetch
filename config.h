267a5588020ca3da76130f9088811c8c38d4ec3a
Iron Oxidizer <60191958+IronOxidizer@users.noreply.github.com>
2020-07-30 17:29:58 -0400

    Update config.h

--- config.h
+++ config.h
@@ -10,9 +10,7 @@
     { "Host: ",       get_host,                true  }, \
     { "Kernel: ",     get_kernel,              true  }, \
     { "Uptime: ",     get_uptime,              false }, \
-    { "Battery: ",    get_battery_percentage,  false }, \
     SPACER \
-    { "Packages: ",   get_packages_pacman,   false }, \
     { "Shell: ",      get_shell,             false }, \
     { "Resolution: ", get_resolution,        false }, \
     { "Terminal: ",   get_terminal,          false }, \
